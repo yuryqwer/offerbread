cfc8e09abe055988cc2907972aae2e8c69cfd19a
yuryqwer <yuryqwer@outlook.com>
2026-07-15 22:24:15 +0800

    seperate asr runtime and service interface

--- internal/asr/asr.go
+++ internal/asr/asr.go
@@ -2,6 +2,16 @@ package asr
 
 import "context"
 
+// ASRRuntime ASR 服务运行时
+type ASRRuntime interface {
+	// Start 启动 ASR 服务
+	Start(ctx context.Context) error
+	// Endpoint 返回 ASR 服务的访问地址
+	Endpoint() string
+	// Stop 停止 ASR 服务
+	Stop() error
+}
+
 // ASRService 将音频流转为文本流
 type ASRService interface {
 	// Transcribe 从 in 读取音频块，将识别结果（完整句子）写入 out
